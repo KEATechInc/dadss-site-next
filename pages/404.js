3f93f98c54fa3ed5f6fb51d46c5fa8b90460e044
tarellmorris <tarellamorris@outlook.com>
2021-02-22 09:09:15 -0500

    adjusted 404 links

--- pages/404.js
+++ pages/404.js
@@ -1,74 +1,74 @@
 import ReactGA from 'react-ga'
 import { useEffect } from 'react'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
+import Link from 'next/link'
 import styled from 'styled-components'
 import {
-  HeadBlock,
-  Header1,
-  Header3,
-  Break,
-  Circle,
-  ContentBlock,
-  Content,
-  Hyperlink,
+	HeadBlock,
+	Header1,
+	Header3,
+	Break,
+	Circle,
+	ContentBlock,
+	Content,
+	Hyperlink,
 } from '../styles/generalStyles'
 
 const DADSSLogo = '/assets/logos/graphic-logo-car.svg'
 
 const PageNotFound = () => {
-  useEffect(() => {
-    ReactGA.initialize('UA-58614629-1')
-    ReactGA.pageview(window.location.pathname)
-  }, [])
+	useEffect(() => {
+		ReactGA.initialize('UA-58614629-1')
+		ReactGA.pageview(window.location.pathname)
+	}, [])
 
-  const description = `Oops. It appears something has gone wrong.`
-  const router = useRouter()
+	const description = `Oops. It appears something has gone wrong.`
 
-  return (
-    <>
-      <Head>
-        <title>DADSS | 404 - Page Not Found</title>
-        <meta name='description' content={description} />
-      </Head>
-      <BodyWrapper>
-        <HeadBlock>
-          <Header1>Page Not Found</Header1>
-          <Break>
-            <hr />
-            <Circle></Circle>
-            <hr />
-          </Break>
-        </HeadBlock>
-        <ContentBlock>
-          <Header3>Oops. Something has gone wrong.</Header3>
-          <img
-            src={DADSSLogo}
-            alt='DADSS Car Logo'
-            onClick={() => router.push('/')}
-          />
-          <Content>
-            Sorry about that. Follow this link to{' '}
-            <Hyperlink onClick={() => router.push('/')}>Return Home</Hyperlink>.
-          </Content>
-        </ContentBlock>
-      </BodyWrapper>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>DADSS | 404 - Page Not Found</title>
+				<meta name='description' content={description} />
+			</Head>
+			<BodyWrapper>
+				<HeadBlock>
+					<Header1>Page Not Found</Header1>
+					<Break>
+						<hr />
+						<Circle></Circle>
+						<hr />
+					</Break>
+				</HeadBlock>
+				<ContentBlock>
+					<Header3>Oops. Something has gone wrong.</Header3>
+					<Link href='/'>
+						<img src={DADSSLogo} alt='DADSS Car Logo' />
+					</Link>
+					<Content>
+						Sorry about that. Follow this link to{' '}
+						<Link href='/'>
+							<Hyperlink>Return Home</Hyperlink>
+						</Link>
+						.
+					</Content>
+				</ContentBlock>
+			</BodyWrapper>
+		</>
+	)
 }
 
 const BodyWrapper = styled.div`
-  margin-top: 85px;
+	margin-top: 85px;
 
-  img {
-    max-height: 150px;
-    width: auto;
-    margin: 25px;
-    cursor: pointer;
-  }
-  p {
-    text-align: center;
-  }
+	img {
+		max-height: 150px;
+		width: auto;
+		margin: 25px;
+		cursor: pointer;
+	}
+	p {
+		text-align: center;
+	}
 `
 
 export default PageNotFound
