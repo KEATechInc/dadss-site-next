2a1be3cde6973f7c61435a15f6a05fc7e5fccfb5
tarellmorris <tarellamorris@outlook.com>
2021-03-08 14:22:54 -0500

    fixed compile crash due to lack of date when calling util

--- util/dateHandler.js
+++ util/dateHandler.js
@@ -1,30 +1,34 @@
 export const formatDate = (date) => {
-	const monthNames = [
-		'January',
-		'February',
-		'March',
-		'April',
-		'May',
-		'June',
-		'July',
-		'August',
-		'September',
-		'October',
-		'November',
-		'December',
-	]
+	if (date) {
+		const monthNames = [
+			'January',
+			'February',
+			'March',
+			'April',
+			'May',
+			'June',
+			'July',
+			'August',
+			'September',
+			'October',
+			'November',
+			'December',
+		]
+	
+		let year = date.slice(0, 4)
+		let month = date.slice(5, 7)
+		let day = date.slice(8, 10)
+	
+		let newDate = `${monthNames[parseInt(month) - 1]} ${day}, ${year}`
+	
+		return newDate
+	}
 
-	let year = date.slice(0, 4)
-	let month = date.slice(5, 7)
-	let day = date.slice(8, 10)
-
-	let newDate = `${monthNames[parseInt(month) - 1]} ${day}, ${year}`
-
-	return newDate
 }
 
 export const getYear = (date) => {
-	let year = date.slice(0, 4)
-
-	return year
+	if (date) {
+		let year = date.slice(0, 4)
+		return year
+	}
 }
