f6ddd357f4b12939a3a32cc37b863f437fcd4482
tarell-morris <77977374+tarell-morris@users.noreply.github.com>
2021-11-30 17:00:42 -0500

    Update index.js

--- pages/webinar/index.js
+++ pages/webinar/index.js
@@ -110,9 +110,9 @@ const Webinar = ({ webinarInfo }) => {
                 src='https://docs.google.com/forms/d/e/1FAIpQLSfZhh67QvXM3jIEEfL3ETjp4rKmybhR7Jkr9hOjaG9eJObPwA/viewform?embedded=true'
                 width='600'
                 height='1286'
-                frameborder='0'
-                marginheight='0'
-                marginwidth='0'>
+                frameBorder='0'
+                marginHeight='0'
+                marginWidth='0'>
                 Loading…
               </iframe>
             </Grid>
