7d354d845d445b031e281680687e6895a8166016
tarell-morris <77977374+tarell-morris@users.noreply.github.com>
2021-11-30 17:16:52 -0500

    Update UrlCheck.js

--- components/Layout/news/UrlCheck.js
+++ components/Layout/news/UrlCheck.js
@@ -2,20 +2,23 @@ import React from 'react'
 import Link from 'next/link'
 
 const UrlCheck = ({ children, link }) => {
-  if (link.slug) {
-    return (
-      <Link href={`/news/updates/${link.slug}`} passHref>
-        {children}
-      </Link>
-    )
+  if (link) {
+    if (link.slug) {
+      return (
+        <Link href={`/news/updates/${link.slug}`} passHref>
+          {children}
+        </Link>
+      )
+    }
+    if (link.url) {
+      return (
+        <a target='_blank' rel='noreferrer' href={link.url}>
+          {children}
+        </a>
+      )
+    }
   }
-  if (link.url) {
-    return (
-      <a target='_blank' rel='noreferrer' href={link.url}>
-        {children}
-      </a>
-    )
-  }
+
   return null
 }
 
